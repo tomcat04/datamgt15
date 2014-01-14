8fde6c9554b96b807ea1894be8fad6f702d86900
ty <HadesSince2012@gmail.com>
2014-01-14 22:41:20 +0800

    add MaterialExceptionLog dao impl

---
+++ dao/src/com/byd/datamgt15/dao/MaterialExceptionLogDaoImpl.java
@@ -0,0 +1,40 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+package com.byd.datamgt15.dao;
+
+import com.byd.datamgt15.domain.MaterialExceptionLog;
+import com.byd.datamgt15.exception.AppException;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author ty
+ */
+@Repository
+public class MaterialExceptionLogDaoImpl extends BaseDaoHibernateImpl<MaterialExceptionLog> implements IMaterialExceptionLogDao {
+
+    @Override
+    public void merge(MaterialExceptionLog bean) {
+        dao.merge(bean);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        MaterialExceptionLog vendor = dao.get(MaterialExceptionLog.class, id);
+        if (vendor != null) {
+            dao.delete(vendor);
+        }
+    }
+
+    @Override
+    public MaterialExceptionLog select(MaterialExceptionLog bean) {
+        if(bean.getId()==null){
+            throw new AppException("参数不正确");
+        }else{
+            return dao.get(MaterialExceptionLog.class, bean.getId());
+        }
+    }
+
+}

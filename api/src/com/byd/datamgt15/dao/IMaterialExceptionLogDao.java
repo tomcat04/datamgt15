4b33f9b3941ddb4a60d07665ae542c030e2c88ce
ty <HadesSince2012@gmail.com>
2014-01-14 22:33:12 +0800

    add a doc

--- api/src/com/byd/datamgt15/dao/IMaterialExceptionLogDao.java
+++ api/src/com/byd/datamgt15/dao/IMaterialExceptionLogDao.java
@@ -12,5 +12,14 @@ import com.byd.datamgt15.domain.MaterialExceptionLog;
  * @author ty
  */
 public interface IMaterialExceptionLogDao extends IBaseDao<MaterialExceptionLog>{
+
+    /**
+     * 使用Id进行Merge
+     * @param bean 
+     */
+    @Override
+    public void merge(MaterialExceptionLog bean);
+    
+    
     
 }

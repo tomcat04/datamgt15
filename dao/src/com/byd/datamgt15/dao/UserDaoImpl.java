57f598c3b7d01cbd19143733dfd3b90d784d527e
ty <hadessince2012@gmail.com>
2014-01-28 18:16:09 +0800

    Todo: login with username or id

--- dao/src/com/byd/datamgt15/dao/UserDaoImpl.java
+++ dao/src/com/byd/datamgt15/dao/UserDaoImpl.java
@@ -43,7 +43,7 @@ public class UserDaoImpl extends BaseDaoHibernateImpl<User> implements IUserDao 
             return dao.get(User.class, bean.getId());
         } else if (bean.getUserName() != null) {
             DetachedCriteria detachedCriteria = DetachedCriteria.forClass(User.class);
-            detachedCriteria.add(Restrictions.eq("userName", bean.getUserName()));
+            detachedCriteria.add(Restrictions.eq("userName", bean.getUserName())); //TODO 用户名还是用户ID
             List findByCriteria = dao.findByCriteria(detachedCriteria);
             if (findByCriteria != null && !findByCriteria.isEmpty()) {
                 return (User) findByCriteria.get(0);

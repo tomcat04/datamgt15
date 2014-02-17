097e066ad1d04cd555f91b8a474fb11c829ab655
ty <hadessince2012@gmail.com>
2014-02-17 16:14:30 +0800

    加服务类方法

--- api/src/com/byd/datamgt15/service/IDocumentService.java
+++ api/src/com/byd/datamgt15/service/IDocumentService.java
@@ -1,10 +1,27 @@
 
 package com.byd.datamgt15.service;
 
+import java.io.File;
+import java.util.List;
+
 /**
  * 文档服务类
  * @author ty
  */
 public interface IDocumentService {
     
+    /**
+     * 根据文件名及物料号，获得文档
+     * @param fileName
+     * @param materialCode 
+     * @return  
+     */
+    public File getDocument(String fileName,String materialCode);
+    
+    /**
+     * 获得物料对应文档名列表
+     * @param materialCode
+     * @return 
+     */
+    public List<String> getDocumentList(String materialCode);
 }

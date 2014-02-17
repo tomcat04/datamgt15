8146336ae6ac1a5449cf629ff315595a9b7ab846
ty <hadessince2012@gmail.com>
2014-02-17 16:14:10 +0800

    加服务类实现包

---
+++ service/src/com/byd/datamgt15/service/DocumentServiceImpl.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.byd.datamgt15.service;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * 文档服务类实现
+ * @author ty
+ */
+public class DocumentServiceImpl implements IDocumentService{
+
+    @Override
+    public File getDocument(String fileName, String materialCode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<String> getDocumentList(String materialCode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

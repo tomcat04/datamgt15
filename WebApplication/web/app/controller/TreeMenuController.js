ae1c50b6c50b7b904ee4d24539242b7c03976104
ty <hadessince2012@gmail.com>
2014-02-20 13:43:41 +0800

    实现菜单点击后tab加

--- WebApplication/web/app/controller/TreeMenuController.js
+++ WebApplication/web/app/controller/TreeMenuController.js
@@ -9,15 +9,23 @@ Ext.define('app.controller.TreeMenuController', {
     init: function() {
         this.control({
             'treeMenu': {
-                render:function(){
-//                    console.info('render');
-                },
-                itemclick:function(view, record, item, index, e, eOpts){
-//                    console.info('itemclick');
-//                        console.info(record);
+                itemclick: function(view, record, item, index, e, eOpts) {
+                    var tabs = Ext.getCmp('myMainPageTabPanel');
+                    var myTabTitle = record.get('text');
+                    var tab = tabs.query('[title=' + myTabTitle + ']');
+                    if (tab.length === 0) {
+                        var newtab = tabs.add({
+                            title: myTabTitle,
+                            closable: true
+                        });
+                        tabs.setActiveTab(newtab);
+                    }else{
+                        tabs.setActiveTab(tab[0]);
+                    }
+
                 }
             }
         });
-        
+
     }
 });  

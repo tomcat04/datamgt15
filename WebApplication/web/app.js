91435ba1fac5ec6b8ecc4e6f4420aca1f76c60e5
ty <hadessince2012@gmail.com>
2014-02-20 09:24:12 +0800

    Ext.Loader换个set写法

--- WebApplication/web/app.js
+++ WebApplication/web/app.js
@@ -3,12 +3,8 @@
  * Each line should be prefixed with  * 
  */
 
-Ext.Loader.setConfig({
-    enabled: true,
-      paths: {  
-          'app': 'app'  
-      }
-});
+  Ext.Loader.setConfig({enabled: true});
+  Ext.Loader.setPath('app', 'app');
 
 
 
